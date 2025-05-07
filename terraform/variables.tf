22123541e29d20b549f4c7116c30072705f6f93f
Erik Ullström <erikullstrom@Eritzy.local>
2025-05-07 23:23:29 +0200

    Uppdaterat main.tf, variables.tf och terraform.tfvars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,40 @@
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Service Principal client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Service Principal client secret"
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID"
+}
+
 variable "location" {
-  type = string
+  type        = string
+  description = "Azure region"
 }
+
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "Target resource group"
 }
+
 variable "kv_name" {
-  type = string
+  type        = string
+  description = "Key Vault name"
 }
+
 variable "kv_secret_name" {
-  type = string
+  type        = string
+  description = "Name of the SSH-key secret in Key Vault"
 }
